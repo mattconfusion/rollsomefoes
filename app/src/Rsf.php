fdeb86b031002a46a2790b37677f8150fb4c3554
Matteo Radice <matteo.radice@voome.it>
2017-05-22 20:18:13 +0200

    =added two modes; added csv columns validation

--- app/src/Rsf.php
+++ app/src/Rsf.php
@@ -12,25 +12,26 @@ class Rsf {
 	const ARRAY_COLUMN_NAME = "Name";
 	const ARRAY_COLUMN_HP = "HP";
 
+	protected $requiredColumns = array(self::CSV_COLUMN_HD, self::CSV_COLUMN_NAME);
 	protected $foes; //arrray
 	protected $foes_count = 0; //total number of foes parsed
 	protected $foes_names;
 
-    /**
-     * Build the Rsf object.
-     * @param [string] $pathToCsv the path to the CSV file containing the foes
-     */
+	/**
+	* Build the Rsf object.
+	* @param [string] $pathToCsv the path to the CSV file containing the foes
+	*/
 	public function __construct($pathToCsv){
 		$this->parseCsvIntoAssocArray($pathToCsv);
 		return $this;
 	}
 
 	/**
-	 * Roll the foe selected by name a number of times
-	 * @param  int $number how many foes?
-	 * @param  string $foe   the foe NAME identifier, strict string comparison
-	 * @return array        [0] holds the assoc array with Name and HD roll; [1] holds all the other info
-	 */
+	* Roll the foe selected by name a number of times
+	* @param  int $number how many foes?
+	* @param  string $foe   the foe NAME identifier, strict string comparison
+	* @return array        [0] holds the assoc array with Name and HD roll; [1] holds all the other info
+	*/
 	public function rollSomeFoes($number,$foe){
 
 		if(!isset($this->foes[$foe])){
@@ -41,8 +42,12 @@ class Rsf {
 
 		for($i=0;$i<$number;$i++){
 			$calc = new Calc($this->foes[$foe][self::CSV_COLUMN_HD]);
+			$result = $calc();
+			if($result <= 0){
+				$result=1;
+			}
 			$rolledFoes[$i][self::ARRAY_COLUMN_NAME] = "$foe #" . $i;
-			$rolledFoes[$i][self::ARRAY_COLUMN_HP] = $calc();
+			$rolledFoes[$i][self::ARRAY_COLUMN_HP] = $result;
 			unset($calc);
 		}
 
@@ -50,17 +55,20 @@ class Rsf {
 	}
 
 	/**
-     * Parse the CSV file into an associative array (thanks to http://stackoverflow.com/a/5674169)
-	 * @param  [string] $pathToCsv Path to the csv file containing the foes list
-	 * @return [Rsf]
-	 */
+	* Parse the CSV file into an associative array (thanks to http://stackoverflow.com/a/5674169)
+	* @param  [string] $pathToCsv Path to the csv file containing the foes list
+	* @return [Rsf]
+	*/
 	protected function parseCsvIntoAssocArray($pathToCsv){
 		$csv = array_map("str_getcsv", file($pathToCsv,FILE_SKIP_EMPTY_LINES));
 		$keys = array_shift($csv);
+		if(array_diff($this->requiredColumns, $keys)){
+    	throw new RsfException('The CSV file must contain at least a column named '.self::CSV_COLUMN_HD.' and a column named '.self::CSV_COLUMN_NAME);
+		}
 		foreach ($csv as $i=>$row) {
-    		$temp_array = array_combine($keys, $row);
-    		$this->foes[$temp_array[self::CSV_COLUMN_NAME]] = $temp_array;
-    		unset($temp_array);
+			$temp_array = array_combine($keys, $row);
+			$this->foes[$temp_array[self::CSV_COLUMN_NAME]] = $temp_array;
+			unset($temp_array);
 		}
 		$this->foes_count = count($this->foes);
 		$this->foes_names = array_keys($this->foes);
@@ -69,9 +77,9 @@ class Rsf {
 	}
 
 	/**
-	 * Debug the foe defs
-	 * @return echo key : json encode of array
-	 */
+	* Debug the foe defs
+	* @return echo key : json encode of array
+	*/
 	public function debugFoesList(){
 		foreach($this->foes as $key=>$value){
 			echo "$key : ".json_encode($value). PHP_EOL;;
--- app/src/RsfCli.php
+++ app/src/RsfCli.php
@@ -49,6 +49,7 @@ class RsfCli {
 				],
 			]);
 
+			//print title
 			$this->climate->draw('rsf-title');
 			//setup the app depending on the args given.
 			$this->checkForHelp();
@@ -56,55 +57,111 @@ class RsfCli {
 			$this->rsf = $this->createRsf($this->climate->arguments->get('file'));
 			$this->climate->br()->whisper('CSV loaded and parsed.');
 			$this->units = $this->climate->arguments->get('units');
-
 			$continue = true;
+
+			//choose mode
+			$input = $this->climate->br()->input('Pick your mode, Sir. Simple(1) or Mixed(2)?');
+			$input->accept(array('1','2'));
+			$input->strict();
+			$mode = $input->prompt();
 
 			//main loop
 			while($continue){
-				try{
-					$this->climate->br()->info($this->rsf->getFoesCount() . ' foe types found.');
-					$this->climate->columns($this->rsf->getFoesNames(), 4);
-
-					$input = $this->climate->br()->input('Pick your FOE, Sir.');
-					$input->accept($this->rsf->getFoesNames());
-					//$input->strict();
-					$foe = $input->prompt();
-					unset($input);
-
-					//ask for the number
-					$input = $this->climate->br()->input('How many of them, kind Sir?');
-					$input->accept(function($response) {
-						return (is_numeric($response));
-					});
-					$input->strict();
-					$number = $input->prompt();
-					unset($input);
-					$this->climate->br();
-
-					//roll dice!
-					$result = $this->rsf->rollSomeFoes($number,$foe);
-
-					//print the results
-					$this->printResults($result,$this->units);
-					unset($result);
-
-					$input = $this->climate->br()->confirm('Would you like something else, Sir?');
-					// Continue? [y/n]
-					if (!$input->confirmed()) {
-						$continue = false;
-						$this->climate->br()->out('Bye Sir.');
-					}
-				}catch(RsfException $e){
-					$this->climate->shout($e->getMessage());
+				$this->switchMode($mode);
+				// Continue? [y/n]
+				$input = $this->climate->br()->confirm('Would you like something else, Sir?');
+				if (!$input->confirmed()) {
+					$continue = false;
+					$this->climate->br()->out('Bye Sir.');
 				}
-
+				unset($input);
 			}
 		}catch(\Exception $e){
 			$this->climate->error($e->getMessage());
 			$this->climate->usage();
 		}
+	}
+
+	/**
+	* Switch between modes given the string parameter
+	* @param  string $mode the string parameter for modes
+	*
+	*/
+	protected function switchMode($mode){
+		switch($mode){
+			case '1':
+			$this->singleMode();
+			break;
+			case '2':
+			$this->mixedMode();
+			break;
+			default:
+			$this->singleMode();
+		}
 
 	}
+
+	/**
+	 * Single mode: user prompted to enter a foe name and after that a number
+	 * @return [type] [description]
+	 */
+	protected function singleMode(){
+		try{
+			$this->printFoesList();
+			$input = $this->climate->br()->input('Pick your FOE, Sir.');
+			$input->accept($this->rsf->getFoesNames());
+			//$input->strict();
+			$foe = $input->prompt();
+			unset($input);
+
+			//ask for the number
+			$input = $this->climate->br()->input('How many of them, kind Sir?');
+			$input->accept(function($response) {
+				return (is_numeric($response));
+			});
+			$input->strict();
+			$number = $input->prompt();
+			unset($input);
+			$this->climate->br();
+
+			//roll dice!
+			$result = $this->rsf->rollSomeFoes($number,$foe);
+			//print the results
+			$this->printResults($result,$this->units);
+			unset($result);
+		}catch(RsfException $e){
+			$this->climate->shout($e->getMessage());
+		}
+	}
+
+	/**
+	 * Mixed mode: the user is asked to enter a list of foes with numbers
+	 * @return [type] [description]
+	 */
+	protected function mixedMode(){
+		try{
+			$this->printFoesList();
+			$input = $this->climate->br()->input('Pick your FOEs, Sir. Describe them to me as name:number and separate them using ;');
+			$mixedFoes = $input->prompt();
+			unset($input);
+			$this->climate->br();
+
+			//parse mixed foes:
+			$singleFoes = explode(';', $mixedFoes);
+			foreach($singleFoes as $sf){
+				$foe = explode(':',$sf);
+				//roll dice!
+				$result = $this->rsf->rollSomeFoes($foe[1],$foe[0]);
+				//print the results
+				$this->printResults($result,$this->units);
+				$this->climate->br();
+			}
+		}catch(RsfException $e){
+			$this->climate->shout($e->getMessage());
+		}
+	}
+
+
 
 	/**
 	* Print the results of Rsf::rollSomeFoes call directly on the console
@@ -123,6 +180,16 @@ class RsfCli {
 		}
 
 		$this->climate->br();
+		return true;
+	}
+
+	/**
+	* Print the foes list in columns
+	* @return bool true
+	*/
+	protected function printFoesList(){
+		$this->climate->br()->info($this->rsf->getFoesCount() . ' foe types found.');
+		$this->climate->columns($this->rsf->getFoesNames(), 4);
 		return true;
 	}
 
